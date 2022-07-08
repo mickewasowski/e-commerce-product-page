c3b112f4a3a24c66835a647dd3b8a9a59e306bb3
Rosen Yorgov <yorgov.h.r@gmail.com>
2022-07-08 22:57:44 +0300

    added CartProvider to index.js

--- e-com-product-react/src/index.js
+++ e-com-product-react/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { CartInfoProvider } from './context/CartContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <CartInfoProvider>
+        <App />
+      </CartInfoProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
